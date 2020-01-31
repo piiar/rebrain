3612ef47b5b6255a5f8777f3cc225b34385d8846
Piia Ruokonen <piiar>
2020-01-31 19:42:18 +0200

    first scripts

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+    private Player player;
+    private float turnSmoothing = 0.1f;
+    private float smoothX = 0;
+    private float smoothY = 0;
+    private float smoothXvelocity = 0;
+    private float smoothYvelocity = 0;
+
+    // Key pressed can only reliably be detected during Update()
+    private bool interactionPressed = false;
+
+    // Start is called before the first frame update
+    void Awake() {
+        player = GetComponent<Player>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1")) {
+            interactionPressed = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            UIManager.instance.Pause();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        // read inputs
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // float mouseX = Input.GetAxis("Mouse X");
+        // float mouseY = Input.GetAxis("Mouse Y");
+
+        // smoothX = Mathf.SmoothDamp(smoothX, mouseX, ref smoothXvelocity, turnSmoothing);
+        // smoothY = Mathf.SmoothDamp(smoothY, mouseY, ref smoothYvelocity, turnSmoothing);
+
+        bool interaction = Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1");
+        Vector3 moveDirection = v * Vector3.forward + h * Vector3.right;
+
+        player.Move(moveDirection, interactionPressed);
+        interactionPressed = false;
+    }
+}
---
+++ Assets/Scripts/ItemTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTrigger : MonoBehaviour {
+    public Item LastItem { get; private set; }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Item")) {
+            print("enter " + other.name);
+            Item item = other.gameObject.GetComponent<Item>();
+            if (item && (item.isCarryable || item.isFixable || item.isUsable)) {
+                LastItem = item;
+            }
+            else {
+                LastItem = null;
+            }
+            print("LastItem: " + (LastItem ? LastItem.name : "null"));
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Item")) {
+            print("exit " + other.name);
+            LastItem = null;
+        }
+    }
+}
