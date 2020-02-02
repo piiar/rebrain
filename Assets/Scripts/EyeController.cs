17d530b28ae6cdc0f288d9d953b80f22871d4755
markku <pastori1989@hotmail.com>
2020-02-02 12:55:57 +0200

    added particles, new eyecontroller

--- Assets/Scripts/EyeController.cs
+++ Assets/Scripts/EyeController.cs
@@ -7,39 +7,52 @@ public class EyeController : MonoBehaviour
 {
 
     public enum EyeStates { Normal, LazyEye, Shrunk, Dilated }
-    public GameObject eye;
-    public GameObject pupil;
+    public GameObject rightEye;
+    public GameObject leftEye;
+    public GameObject rightPupil;
+    public GameObject leftPupil;
     public EyeStates eyeState = EyeStates.Normal;
-    Tweener eyeTween;
-    Tweener pupilTween;
+
+    Tweener eyeTweenL;
+    Tweener eyeTweenR;
+    Tweener pupilTweenL;
+    Tweener pupilTweenR;
+
     private float randomEyePosDiff = 0.4f;
     private float currentDilation = 1f;
 
     void Start()
     {
         Vector3 targetPos = getRandomTargetPosition ();
-        eyeTween = eye.transform.DOMove(targetPos, 1f, false).OnComplete(posCompleteFunction);
+        eyeTweenR = rightEye.transform.DOLocalMove(targetPos, 1f, false).SetLoops(-1, LoopType.Yoyo).OnComplete(posCompleteFunction);
+        eyeTweenL = leftEye.transform.DOLocalMove(targetPos, 1f, false).SetLoops(-1, LoopType.Yoyo).OnComplete(posCompleteFunction);
 
-        Vector3 targetScale = getRandomTargetScale ();
-        pupilTween = pupil.transform.DOScale(targetScale, 1f).OnComplete(scaleCompleteFunction);
+        leftPupil.transform.localScale = Vector3.one;
+        rightPupil.transform.localScale = Vector3.one;
+
+        Vector3 targetScale = new Vector3(10f, 10, 1f);
+        pupilTweenL = leftPupil.transform.DOScale(targetScale, 1f).SetLoops(-1, LoopType.Yoyo);
+        pupilTweenR = rightPupil.transform.DOScale(targetScale, 1f).SetLoops(-1, LoopType.Yoyo);
     }
 
     void posCompleteFunction () {
         Vector3 targetPos = getRandomTargetPosition ();
-        eyeTween = eye.transform.DOMove(targetPos, 1f, false).OnComplete(posCompleteFunction);
+        eyeTweenR.ChangeEndValue(targetPos);
+        eyeTweenL.ChangeEndValue(targetPos);
     }
 
-    void scaleCompleteFunction () {
-        Vector3 targetPos = getRandomTargetPosition ();
-        pupilTween = eye.transform.DOMove(targetPos, 1f, false).OnComplete(scaleCompleteFunction);
-    }
+    // void scaleCompleteFunction () {
+    //     Vector3 targetPos = getRandomTargetPosition ();
+    //     pupilTweenR = rightPupil.transform.DOMove(targetPos, 2f, false).OnComplete(scaleCompleteFunction);
+    //     pupilTweenL = rightPupil.transform.DOMove(targetPos, 2f, false).OnComplete(scaleCompleteFunction);
+    // }
 
     Vector3 getRandomTargetPosition () {
         return transform.localPosition + new Vector3 (Random.Range(-randomEyePosDiff, randomEyePosDiff), Random.Range(-randomEyePosDiff, randomEyePosDiff), 0);
     }
 
-    Vector3 getRandomTargetScale () {
-        currentDilation = Random.Range(5f, 15f);
-        return new Vector3 (currentDilation, currentDilation, 0);
-    }
+    // Vector3 getRandomTargetScale () {
+    //     currentDilation = Random.Range(5f, 15f);
+    //     return new Vector3 (currentDilation, currentDilation, 0);
+    // }
 }
